5d8830aea958fa99106836a2d2c9b0ca288d1b76
andreykoleber <andrei.koleber@gmail.com>
2018-05-16 23:22:48 +0600

    Add custom view

--- app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawFragment.java
+++ app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawFragment.java
@@ -1,17 +1,19 @@
 package com.bignerdranch.android.draganddraw;
 
+import android.nfc.Tag;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class DragAndDrawFragment extends ListFragment {
+public class DragAndDrawFragment extends Fragment {
+
+
 
     public static DragAndDrawFragment newInstance() {
         return new DragAndDrawFragment();
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -20,4 +22,7 @@ public class DragAndDrawFragment extends ListFragment {
 
         return view;
     }
+
+
+
 }
